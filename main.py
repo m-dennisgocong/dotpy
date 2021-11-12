443cc3356c3bc6d172c16f08834aebbb54798428
Dennis Goc-ong <m.dennisgoc-ong>
2021-11-12 18:06:21 +0700

    modified Menu bar class and created new classes FramController and MainFrame and delete the other classes

--- main.py
+++ main.py
@@ -1,121 +1,141 @@
+import os
 import tkinter as tk
 from tkinter import ttk,filedialog
-class TreeviewFrame(ttk.Frame):
+from tkinter.filedialog import asksaveasfilename,askopenfilename
+
+class MainFrame(tk.Frame):
     def __init__(self,container):
         super().__init__(container)
-        self.columnconfigure(0, weight=3)
-        self.rowconfigure(0,weight=3)
         self.tree = ttk.Treeview(container, show="tree")
-        self.ybar = ttk.Scrollbar(container,orient = "vertical", command = self.tree.yview)
+        #self.ybar = ttk.Scrollbar(container,orient = "vertical", command = self.tree.yview)
         style = ttk.Style()
         style.configure("Treeview",background="azure2",foreground="black",fieldbackground="azure2")
-        self.tree.configure(yscroll = self.ybar.set)
-    def select_folder(self):
-        directory = filedialog.askdirectory()
-        #print(directory)
-        self.tree.heading("#0", text="Dir: "+directory,anchor="w")
-        path=os.path.abspath(directory)
-        node = self.tree.insert("","end",text=path,open=True,tag="top_dir")
-        def traverse_dir(parent,path):
-            for d in os.listdir(path):
-                full_path=os.path.join(path,d)
-                isdir = os.path.isdir(full_path)
-                id=self.tree.insert(parent,'end',text=d,open=False,values=full_path)
-                if isdir:
-                    traverse_dir(id,full_path)
-        traverse_dir(node,path)
-        self.ybar.grid(row=0,column=1,sticky="nsw")
-        self.tree.grid(column=0,sticky="NSWE",row=0,rowspan=2)
-        def selectItem():
-            print("work")
-            try:
-                curItem = self.tree.focus()
-                sltpath = self.tree.item(curItem)["values"]
-                file_selected = os.path.abspath(sltpath[0])
-                name_of_file = self.tree.item(curItem)["text"]
-                filename = os.path.basename(file_selected)
-                isfile = os.path.isdir(file_selected)
-                if not isfile:
-                    with open(file_selected,"r") as file:
-                        code = file.read()
-                    create_file(code,filename,file_selected)
-                else:
-                    pass
-            except:
-                pass
 
-        self.tree.bind("<Double-1>", selectItem())
+        #text_area = tk.Text(container)
+        #text_area.insert("1.0","")
+    #    text_area.grid(column=2, row=0, sticky="WNES")
+        self.nb = ttk.Notebook(container)
+        self.nb.grid(column=1, row=0, sticky="WNES")
 
-class TextEdit(ttk.Frame):
-    def __init__(self,container):
-        super().__init__(container)
-        self.columnconfigure(0,weight = 3)
-        self.columnconfigure(0,weight = 1)
-        self.__create_widgets()
-    def __create_widgets(self):
-        text_area = tk.Text(self)
-        text_area.insert("1.0","")
-        text_area.grid(column=0, row=0, sticky="NEWS")
+
+        text_result = tk.Text(container)
+        text_result.grid(column=1, row=1, sticky="WNES")
+        self.grid(column=0, row=0,sticky="NEWS")
+
+class FrameController(MainFrame):
+    def __init__(self, main):
+        super().__init__(main)
+
+    def openfolder(self):
+        try:
+
+            directory = filedialog.askdirectory()
+            path=os.path.abspath(directory)
+            file_names = path.split("/")
+            #self.frame_area.tree.heading("#0", text=path,anchor="w")
+            node = self.tree.insert("","end",text=file_names[-1],open=True,tag="top_dir")
+            def traverse_dir(parent,path):
+                for d in os.listdir(path):
+                    full_path=os.path.join(path,d)
+                    isdir = os.path.isdir(full_path)
+                    id=self.tree.insert(parent,'end',text=d,open=False,values=full_path)
+                    if isdir:
+                        traverse_dir(id,full_path)
+            traverse_dir(node,path)
+                #self.frame_area.tree.configure(yscroll = self.frame_area.ybar.set)
+            self.tree.grid(column=0,sticky="NEWS",row=0,rowspan=2)
+
+                #self.frame_area.ybar.grid(row=0,column=0,rowspan=2,sticky="WNES")
+        except FileNotFoundError as error:
+            showerror(title='Error', message=error)
+
+    def create_file(self,content = "",title="Untitled", filepath = ""):
+        text_area = tk.Text(self.nb, wrap="none")
+        self.nb.add(text_area)
+
 class MenuBar(tk.Menu):
-    def __init__(self, parent):
-        tk.Menu.__init__(self, parent)
+    def __init__(self, main):
+        super().__init__(main)
 
-        #File Bar
-        file_bar = tk.Menu(self, tearoff=0) #tearoff for no line
-        file_bar.add_command(label = "New File")
-        file_bar.add_command(label = "Open File")
-        file_bar.add_command(label = "Open Folder")
-        file_bar.add_separator()
-        file_bar.add_command(label = "Save")
-        file_bar.add_command(label = "Save As..")
-        file_bar.add_command(label = "Close Tab")
-        file_bar.add_separator()
-        file_bar.add_command(label = "Quit", command = self.quit)
-        self.add_cascade(label = "File", menu = file_bar) # File bar
-        #Edit Bar
-        edit_bar = tk.Menu(self,tearoff=0)
-        edit_bar.add_command(label = "Undo")
-        edit_bar.add_separator()
-        edit_bar.add_command(label = "Cut")
-        edit_bar.add_command(label = "Copy")
-        edit_bar.add_command(label = "Paste")
-        edit_bar.add_separator()
-        edit_bar.add_command(label = "Delete")
-        edit_bar.add_command(label = "Select All")
-        self.add_cascade(label = "Edit", menu = edit_bar)
+        # file bar list
+        file_list = ["New File", "Open File", "Open Folder", "Save", "Save As..",
+        "Close Tab", "Quit"]
+
+        # create file bar menu
+        self.file_bar = tk.Menu(main, tearoff=0) # tearoff for no line
+
+        # add the file_list to the file_bar menu
+        for i in file_list:
+            self.file_bar.add_command(label = i)
+
+        # add the file menu to the menubar
+        self.add_cascade(label = "File", menu = self.file_bar)
+
+        # create separator
+        self.file_bar.insert_separator(3)
+        self.file_bar.insert_separator(7)
+
+        # edit bar list
+        edit_list = ["Undo","Copy","Paste","Delete","Select All"]
+
+        # create edit bar
+        self.edit_bar = tk.Menu(self,tearoff=0)
+        for i in edit_list:
+            self.edit_bar.add_command(label = i)
+
+        # create separator
+        self.edit_bar.insert_separator(1)
+        self.edit_bar.insert_separator(4)
+        self.add_cascade(label = "Edit", menu = self.edit_bar)
 
         #Run Bar
-        run_bar = tk.Menu(self,tearoff=0)
-        run_bar.add_command(label = "Run")
-        self.add_cascade(label = "Run", menu = run_bar)
+        self.run_bar = tk.Menu(self,tearoff=0)
+        self.run_bar.add_command(label = "Run")
+        self.add_cascade(label = "Run", menu = self.run_bar)
 
     def quit(self):
         sys.exit(0)
 
 class MainApp(tk.Tk):
-
     def __init__(self):
         super().__init__()
         self.title("dotpy")
-        self.geometry("1000x650")
-        self.__create_widgets()
+        self.geometry("850x650")
+        #self.resizable(False, False)
 
-        #Layout on the root
-        self.columnconfigure(0,weight = 1)
-        self.columnconfigure(1,weight = 5)
+        # layout on the root/main
+        self.columnconfigure(0,weight = 1) #For Scroll Bar
+        self.columnconfigure(1,weight = 5) #For Tree View
+        #self.columnconfigure(2,weight = 5) #For text edit and result
 
-        menu_bar = MenuBar(self) #Menu bar for File,Edit and Run File
-        self.config(menu = menu_bar)
+        # FrameController(MainFrame)
+        self.frame_control = FrameController(self)
 
-    def __create_widgets(self):
-        tree_view = TreeviewFrame(self)
-        tree_view.grid(column = 0, row = 0)
+        # create a Menubar
+        self.menu_bar = MenuBar(self)
+        self.config(menu = self.menu_bar)
 
-        text_edit_area = TextEdit(self)
-        tree_view.grid(column = 1, row = 0)
+        # file bar fucntions call
+        self.menu_bar.file_bar.entryconfig(0, command=self.creat_t , accelerator = "Ctrl+N")
+        self.menu_bar.file_bar.entryconfig(1, command=self.creat_t , accelerator = "Ctrl+O")
+        self.menu_bar.file_bar.entryconfig(2, command=self.creat_t , accelerator = "Ctrl+Shift+O")
+        self.menu_bar.file_bar.entryconfig(4, command=self.creat_t , accelerator = "Ctrl+S")
+        self.menu_bar.file_bar.entryconfig(5, command=self.creat_t , accelerator = "Crt+Shift+S")
+        self.menu_bar.file_bar.entryconfig(6, command=self.creat_t , accelerator = "Crt+W")
+        self.menu_bar.file_bar.entryconfig(8, command=self.creat_t , accelerator = "Crt+Q")
+        # edit bar function call
+        self.menu_bar.edit_bar.entryconfig(0, command=self.creat_t , accelerator = "Ctrl+Z")
+        self.menu_bar.edit_bar.entryconfig(2, command=self.creat_t , accelerator = "Ctrl+C")
+        self.menu_bar.edit_bar.entryconfig(3, command=self.creat_t , accelerator = "Ctrl+V")
+        self.menu_bar.edit_bar.entryconfig(5, command=self.creat_t)
+        self.menu_bar.edit_bar.entryconfig(6, command=self.creat_t , accelerator = "Ctrl+A")
 
-    def open_folder(self):
-        t = TreeviewFrame.select_folder()
+        # run bar function call
+        self.menu_bar.run_bar.entryconfig(0, command=self.creat_t, accelerator = "Ctrl+Shift+B")
+    def etry(self):
+        self.frame_control.openfolder()
+    def creat_t(self):
+        self.frame_control.create_file()
 
 if __name__ == "__main__":
     main = MainApp()
