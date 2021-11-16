1f43592f8c6e5bd0f261135ed094a084f88f6c29
Dennis Goc-ong <m.dennisgoc-ong>
2021-11-16 20:23:26 +0700

    Modified FrameController Methods

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import subprocess
 import tkinter as tk
-from tkinter import ttk,filedialog
+import save_load as sl
+from tkinter import ttk,filedialog,messagebox
 from tkinter.filedialog import asksaveasfilename,askopenfilename
 
 class MainFrame(tk.Frame):
@@ -15,12 +17,15 @@ class MainFrame(tk.Frame):
         style.configure("Treeview",background="azure2",foreground="black",fieldbackground="azure2")
 
         self.nb = ttk.Notebook(container)
-        welcome_text = tk.StringVar()
 
+        """
+        welcome_text = tk.StringVar()
         self.welcome_title = tk.Label(self.nb, textvariable=welcome_text,justify="center", height="25", width="25")
         welcome_text.set("Welcome \nto \ndotpy")
-
-        self.nb.add(self.welcome_title)
+        """
+        #need to get back here after done on save method
+        #text_area = tk.Text(self.nb, wrap = "none")
+        #self.nb.add(text_area)
         self.nb.grid(column=1, row=0, sticky="WNES")
 
         text_result = tk.Text(container)
@@ -30,7 +35,6 @@ class MainFrame(tk.Frame):
 class FrameController(MainFrame):
     def __init__(self, main):
         super().__init__(main)
-
     #create text in nb
     def create_file(self,content = "",title="Untitled", filepath = ""):
         text_area = tk.Text(self.nb, wrap="none")
@@ -54,13 +58,14 @@ class FrameController(MainFrame):
             code = file.read()
         self.create_file(code,filename,path)
 
+    # open folder
     def openfolder(self):
         try:
             directory = filedialog.askdirectory()
             path=os.path.abspath(directory)
-            file_names = path.split("/")
-            #self.frame_area.tree.heading("#0", text=path,anchor="w")
-            node = self.tree.insert("","end",text=file_names[-1],open=True,tag="top_dir")
+            folder_name = path.split("/")
+            self.tree.heading("#0", text="Dir: "+directory,anchor="w")
+            node = self.tree.insert("","end",text=folder_name[-1],open=True,tag="top_dir")
             def traverse_dir(parent,path):
                 for d in os.listdir(path):
                     full_path=os.path.join(path,d)
@@ -75,6 +80,24 @@ class FrameController(MainFrame):
                 #self.frame_area.ybar.grid(row=0,column=0,rowspan=2,sticky="WNES")
         except FileNotFoundError as error:
             showerror(title='Error', message=error)
+
+    # select from opened folder
+    def select_file(self):
+        try:
+            curItem = self.tree.focus()
+            sltpath = self.tree.item(curItem)["values"]
+            file_selected = os.path.abspath(sltpath[0])
+            name_of_file = self.tree.item(curItem)["text"]
+            filename = os.path.basename(file_selected)
+            isfile = os.path.isdir(file_selected)
+            if not isfile:
+                with open(file_selected,"r") as file:
+                    code = file.read()
+                self.create_file(code,filename,file_selected)
+            else:
+                pass
+        except:
+            pass
 
     # get the selected tab in notebook
     def select_nb_tab(self):
@@ -122,7 +145,63 @@ class FrameController(MainFrame):
             self.content_data[str(current_tab)][0] = hash(content)
         else:
             self.save_as()
-        self.check_for_changes()
+        #self.check_for_changes()
+
+    def close_current_tab(self):
+        current = self.select_nb_tab()
+        if self.current_tab_nsave() and not self.confirm_close():
+            return
+        if len(self.nb.tabs()) == 1:#create notebook if close all tab
+            self.create_file()
+        self.nb.forget(current)
+
+    #check if current tab not save
+    def current_tab_nsave(self):
+        get_content_text = self.select_nb_tab()
+        content = get_content_text.get("1.0","end-1c")
+        return hash(content) != self.content_data[str(get_content_text)][0] #return true if tab is not save_as
+
+    #check confim to close
+    def confirm_close(self):
+        return messagebox.askyesno(
+            message="Your changes will be lost if you close without saving. Are you sure you want to close?",
+            icon="question",
+            title="Unsave changes"
+        )
+
+    #Close All
+    def confirm_quit(self, root):
+        savetabs = list() # save the notebook/tabs
+        unsave = False
+        unsavetabs = list() #unsave notebook/tabs
+
+        #check all notebook
+        for tab in self.nb.tabs():
+            currenttab = main.nametowidget(tab)
+            content = currenttab.get("1.0","end-1c")
+            if hash(content) != self.content_data[str(currenttab)][0]: #check the unsave file
+                unsave = True
+                unsavetabs.append(self.content_data[str(currenttab)][1]) #stored to unsavetabs
+
+            savetabs.append(self.content_data[str(currenttab)][2]) #store to savetabs
+
+        #confirmation for quiting
+        if unsave:
+            tabsname = ""
+            for x in unsavetabs:
+                tabsname = tabsname + x + ", "
+            confirm = messagebox.askyesno(
+                message = "{} has changes. Are you sure you want to quit without saving?".format(tabsname),
+                icon = "question",
+                title = "Confirm Quit"
+            )
+            #save_load data/ save the tabs
+            if not confirm :
+                return
+        with open("save_load.py","w") as f:
+            f.write("listtabs =" + str(savetabs))
+        return True
+
 
 class MenuBar(tk.Menu):
     def __init__(self, main):
@@ -164,9 +243,6 @@ class MenuBar(tk.Menu):
         self.run_bar.add_command(label = "Run")
         self.add_cascade(label = "Run", menu = self.run_bar)
 
-    def quit(self):
-        sys.exit(0)
-
 class MainApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -186,11 +262,11 @@ class MainApp(tk.Tk):
         # file bar command
         self.menu_bar.file_bar.entryconfig(0, command=self.creat_f , accelerator = "Ctrl+N")
         self.menu_bar.file_bar.entryconfig(1, command=self.open_f , accelerator = "Ctrl+O")
-        self.menu_bar.file_bar.entryconfig(2, command=self.creat_f , accelerator = "Ctrl+Shift+O")
+        self.menu_bar.file_bar.entryconfig(2, command=self.openfolder , accelerator = "Ctrl+Shift+O")
         self.menu_bar.file_bar.entryconfig(4, command=self.save_f , accelerator = "Ctrl+S")
         self.menu_bar.file_bar.entryconfig(5, command=self.save_as , accelerator = "Crt+Shift+S")
-        self.menu_bar.file_bar.entryconfig(6, command=self.creat_f , accelerator = "Crt+W")
-        self.menu_bar.file_bar.entryconfig(8, command=self.creat_f , accelerator = "Crt+Q")
+        self.menu_bar.file_bar.entryconfig(6, command=self.close_tab , accelerator = "Crt+W")
+        self.menu_bar.file_bar.entryconfig(8, command=self.quit , accelerator = "Crt+Q")
         # edit bar command
         self.menu_bar.edit_bar.entryconfig(0, command=self.creat_f , accelerator = "Ctrl+Z")
         self.menu_bar.edit_bar.entryconfig(2, command=self.creat_f , accelerator = "Ctrl+C")
@@ -199,7 +275,11 @@ class MainApp(tk.Tk):
         self.menu_bar.edit_bar.entryconfig(6, command=self.creat_f , accelerator = "Ctrl+A")
 
         self.menu_bar.run_bar.entryconfig(0, command=self.creat_f, accelerator = "Ctrl+Shift+B")
+
+        self.frame_control.tree.bind("<Double-1>", lambda e: self.frame_control.select_file())
         self.bind("<KeyPress>", lambda e: self.frame_control.change_checker())
+        self.bind("<Control-w>", lambda e: self.frame_control.close_current_tab())
+        self.protocol("WM_DELETE_WINDOW",self.quit)
 
     def open_f(self):
         self.frame_control.open_file()
@@ -211,6 +291,11 @@ class MainApp(tk.Tk):
         self.frame_control.save_file()
     def save_as(self):
         self.frame_control.save_as()
+    def close_tab(self):
+        self.frame_control.close_current_tab()
+    def quit(self):
+        if self.frame_control.confirm_quit(self) is True:
+            self.destroy()
 
 if __name__ == "__main__":
     main = MainApp()
